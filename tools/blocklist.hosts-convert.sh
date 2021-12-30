93c55e120a406dd5bfacacfb0f7b7d6fd4d7330d
Peter Lerner <peter@lerner.de>
2021-12-30 13:45:04 +0100

    addes script tools/blocklist.hosts-convert.sh

--- tools/blocklist.hosts-convert.sh
+++ tools/blocklist.hosts-convert.sh
@@ -1,4 +1,5 @@
 echo "return{" >blocklist.lua
-cat ../hosts | grep '^0\.0\.0\.0' |awk '{ print "\""$2"\"," }' >>blocklist.lua
+cat ../hosts     | grep '^0\.0\.0\.0' |awk '{ print "\""$2"\"," }' >>blocklist.lua
+cat ../hosts-PLE | grep '^0\.0\.0\.0' |awk '{ print "\""$2"\"," }' >>blocklist.lua
 echo "}" >>blocklist.lua
 
