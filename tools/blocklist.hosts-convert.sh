a0929e4674ce238e3f4ef775a98266c9db9a50c8
Peter Lerner <peter@lerner.de>
2022-01-07 19:27:00 +0100

    blocklist.lua => blocklist-domains.lua

--- tools/blocklist.hosts-convert.sh
+++ tools/blocklist.hosts-convert.sh
@@ -1,5 +1,5 @@
-echo "return{" >blocklist.lua
-cat ../hosts     | grep '^0\.0\.0\.0' |awk '{ print "\""$2"\"," }' >>blocklist.lua
-cat ../hosts-PLE | grep '^0\.0\.0\.0' |awk '{ print "\""$2"\"," }' >>blocklist.lua
-echo "}" >>blocklist.lua
+echo "return{" >blocklist-domains.lua
+cat ../hosts     | grep '^0\.0\.0\.0' |awk '{ print "\""$2"\"," }' >>blocklist-domains.lua
+cat ../hosts-PLE | grep '^0\.0\.0\.0' |awk '{ print "\""$2"\"," }' >>blocklist-domains.lua
+echo "}" >>blocklist-domains.lua
 
